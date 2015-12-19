56b8b48953187595af008ce36e74db4b66ac91e3
amanmehara <amanmehara@hotmail.com>
2015-12-19 17:32:09 +0530

    Language Page Redesigned.
    Images provided for each programming language.

--- android/src/main/java/com/amanmehara/programming/android/LanguageAdapter.java
+++ android/src/main/java/com/amanmehara/programming/android/LanguageAdapter.java
@@ -4,11 +4,14 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import org.json.JSONArray;
 import org.json.JSONException;
 
 public class LanguageAdapter extends RecyclerView.Adapter<LanguageAdapter.ViewHolder> {
+
+    private String language;
 
     private JSONArray mDataset;
 
@@ -36,10 +39,62 @@ public class LanguageAdapter extends RecyclerView.Adapter<LanguageAdapter.ViewHo
     @Override
     public void onBindViewHolder(LanguageAdapter.ViewHolder viewHolder, int i) {
         try {
-            viewHolder.mTextView.setText(mDataset.getString(i));
+            language = mDataset.getString(i);
         } catch (JSONException e) {
             e.printStackTrace();
         }
+
+        switch (language) {
+            case "c":
+                viewHolder.mImageView.setImageResource(R.drawable.c);
+                viewHolder.mTextView.setText("C");
+                break;
+            case "cpp":
+                viewHolder.mImageView.setImageResource(R.drawable.cpp);
+                viewHolder.mTextView.setText("C++");
+                break;
+            case "csharp":
+                viewHolder.mImageView.setImageResource(R.drawable.csharp);
+                viewHolder.mTextView.setText("C#");
+                break;
+            case "fsharp":
+                viewHolder.mImageView.setImageResource(R.drawable.fsharp);
+                viewHolder.mTextView.setText("F#");
+                break;
+            case "groovy":
+                viewHolder.mImageView.setImageResource(R.drawable.groovy);
+                viewHolder.mTextView.setText("Groovy");
+                break;
+            case "java":
+                viewHolder.mImageView.setImageResource(R.drawable.java);
+                viewHolder.mTextView.setText("Java");
+                break;
+            case "javascript":
+                viewHolder.mImageView.setImageResource(R.drawable.javascript);
+                viewHolder.mTextView.setText("JavaScript");
+                break;
+            case "php":
+                viewHolder.mImageView.setImageResource(R.drawable.php);
+                viewHolder.mTextView.setText("PHP");
+                break;
+            case "python":
+                viewHolder.mImageView.setImageResource(R.drawable.python);
+                viewHolder.mTextView.setText("Python");
+                break;
+            case "scala":
+                viewHolder.mImageView.setImageResource(R.drawable.scala);
+                viewHolder.mTextView.setText("Scala");
+                break;
+            case "sql":
+                viewHolder.mImageView.setImageResource(R.drawable.sql);
+                viewHolder.mTextView.setText("SQL");
+                break;
+            default:
+                viewHolder.mImageView.setImageResource(R.drawable.ic_circle_logo);
+                viewHolder.mTextView.setText(language);
+        }
+
+
     }
 
     @Override
@@ -53,10 +108,12 @@ public class LanguageAdapter extends RecyclerView.Adapter<LanguageAdapter.ViewHo
 
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         public TextView mTextView;
+        public ImageView mImageView;
 
         public ViewHolder(View v) {
             super(v);
             mTextView = (TextView) v.findViewById(R.id.language_name);
+            mImageView = (ImageView) v.findViewById(R.id.language_image);
             v.setOnClickListener(this);
         }
 
